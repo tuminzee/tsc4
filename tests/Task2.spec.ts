38965f6a17d732e5d72e7c62d0584ec84b798578
tuminzee <t.sheth33@gmail.com>
2023-08-09 16:53:19 +0200

    add task 2

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,8 +1,8 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
-import { Task2 } from '../wrappers/Task2';
+import {Blockchain, SandboxContract} from '@ton-community/sandbox';
+import {Cell, toNano} from 'ton-core';
+import {Task2} from '../wrappers/Task2';
 import '@ton-community/test-utils';
-import { compile } from '@ton-community/blueprint';
+import {compile} from '@ton-community/blueprint';
 
 describe('Task2', () => {
     let code: Cell;
@@ -34,5 +34,54 @@ describe('Task2', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task2 are ready to use
+    });
+
+    it('2x3:3x2', async () => {
+        const a = [[1, 2, 3], [4, 5, 6]];
+        const b = [[7, 8], [9, 10], [11, 12]];
+
+        const result = await task2.getMatrixMultiplier(a, b);
+        expect(result).toBe(true);
+    });
+
+    it('4x4:4x4', async () => {
+        const a = [[1, 2, 3, 4], [4, 5, 6, 7]];
+        const b = [[7, 8, 9, 10], [9, 10, 11, 12]];
+
+        const result = await task2.getMatrixMultiplier(a, b);
+        expect(result).toBe(true);
+    });
+
+    it('5x5:5x5', async () => {
+        const a = task2.generateDummyMatrix(5, 5);
+        const b = task2.generateDummyMatrix(5, 5);
+        console.log(a);
+        console.log(b);
+
+        const result = await task2.getMatrixMultiplier(a, b);
+        expect(result).toBe(true);
+    });
+
+    it('10x10:10x10', async () => {
+        const a = task2.generateDummyMatrix(10, 10);
+        const b = task2.generateDummyMatrix(10, 10);
+        console.log(a);
+        console.log(b);
+
+        const result = await task2.getMatrixMultiplier(a, b);
+        expect(result).toBe(true);
+    });
+
+    it('should pass', async () => {
+        for (let i = 1; i < 30; i++) {
+            console.log('test for', i);
+            const a = task2.generateDummyMatrix(i, i);
+            const b = task2.generateDummyMatrix(i, i);
+            // console.log(a);
+            // console.log(b);
+
+            const result = await task2.getMatrixMultiplier(a, b);
+            expect(result).toBe(true);
+        }
     });
 });
--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,4 +1,15 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+    Tuple,
+    TupleItemInt
+} from 'ton-core';
 
 export type Task2Config = {};
 
@@ -7,7 +18,8 @@ export function task2ConfigToCell(config: Task2Config): Cell {
 }
 
 export class Task2 implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
+    }
 
     static createFromAddress(address: Address) {
         return new Task2(address);
@@ -26,4 +38,77 @@ export class Task2 implements Contract {
             body: beginCell().endCell(),
         });
     }
+
+    static solve(a: number[][], b: number[][]): number[][] {
+        const result = [];
+        for (let i = 0; i < a.length; i++) {
+            const row = [];
+            for (let j = 0; j < b[i].length; j++) {
+                let s = 0;
+                for (let k = 0; k < b.length; k++) {
+                    s += a[i][k] * b[k][j];
+                }
+                row.push(s);
+            }
+            result.push(row);
+        }
+        return result;
+    }
+
+    static serializeToTuple(arr: number[][]): Tuple {
+        const matrix: Tuple[] = [];
+        for (let i = 0; i < arr.length; i++) {
+            const row: TupleItemInt[] = [];
+            for (let j = 0; j < arr[i].length; j++) {
+                row.push({ type: 'int', value: BigInt(arr[i][j]) });
+            }
+            matrix.push({ type: 'tuple', items: row });
+        }
+        return { type: 'tuple', items: matrix };
+    }
+
+    async getMatrixMultiplier(provider: ContractProvider, a: number[][], b: number[][]): Promise<boolean> {
+        const { stack } = await provider.get('matrix_multiplier', [
+            Task2.serializeToTuple(a), Task2.serializeToTuple(b)
+        ]);
+
+        const expected = Task2.solve(a, b);
+        const result = stack.readTuple();
+
+
+
+        for (let i = 0; i < expected.length; i++) {
+            const row = result.readTuple();
+            for (let j = 0; j < expected[i].length; j++) {
+                const val = row.readNumber();
+                // console.log({
+                //     val,
+                //     expected: expected[i][j]
+                // })
+                if (val != expected[i][j]) {
+                    return false;
+                }
+            }
+            expect(row.remaining).toBe(0);
+        }
+        expect(result.remaining).toBe(0);
+
+        return true;
+    }
+
+
+    generateDummyMatrix(rows: number, cols: number) {
+        const matrix: number[][] = [];
+
+        for (let i = 1; i <= rows; i++) {
+            const row: number[] = [];
+            for (let j = 1; j <= cols; j++) {
+                row.push((i - 1) * cols + j);
+            }
+            matrix.push(row);
+        }
+
+        return matrix;
+    }
+
 }
